c2e767f8621c16b67917c526ccb8e8bbe96f77e8
Garrett Luecke <garretteluecke@gmail.com>
2020-01-19 19:18:22 -0600

    searches dorodash for store and delivery fee

--- src/controllers/doordashController.js
+++ src/controllers/doordashController.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 
 exports.searchDoordash = async (req, res) => {
-  await (async () => {
+  let ddSearch = await (async () => {
     const browser = await puppeteer.launch({
       // headless: false,
       defaultViewport: null,
@@ -11,12 +11,13 @@ exports.searchDoordash = async (req, res) => {
         "--disable-setuid-sandbox"
       ]
     });
+
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
     await page.goto("https://doordash.com", { waitUntil: "networkidle0" });
     await page.waitForSelector("input");
 
-    await page.type("input", "526 Market St, Philadelphia, PA", { delay: 200 });
+    await page.type("input", req.query.location, { delay: 200 });
     await page.keyboard.press("ArrowDown");
     await page.keyboard.press("Enter");
 
@@ -24,13 +25,40 @@ exports.searchDoordash = async (req, res) => {
     await page.waitFor(5000);
 
     await page.waitForSelector("input");
-    await page.type("input", "Five Guys");
+    await page.type("input", req.query.store);
     await page.keyboard.press("ArrowDown");
     await page.keyboard.press("Enter");
 
-    page.close();
-    browser.close();
+    let doorDashRes = await page.waitForResponse(response => {
+      return (
+        response.url().includes("graphql") &&
+        response.request().method() === "POST"
+      );
+    });
+
+    ddJson = await doorDashRes.json();
+
+    await page.close();
+    await browser.close();
+
+    let found = ddJson.data.storeSearch.stores.find(store =>
+      store.name.includes(req.query.store)
+    );
+
+    if (found) {
+      return {
+        store: req.query.store,
+        found: true,
+        deliveryFee: found.deliveryFee
+      };
+    } else {
+      return {
+        store: req.query.store,
+        found: false,
+        deliveryFee: null
+      };
+    }
   })();
 
-  res.json({ found: false, deliveryFee: null });
+  res.json(ddSearch);
 };
